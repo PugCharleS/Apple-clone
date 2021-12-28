e57862a9a019716e1a1a1867673e7d2afa414d87
Carlos Solis <cmst010@gmail.com>
2021-12-28 16:40:17 -0700

    ItemCount

---
+++ prueba/src/components/ItemCount/ItemCount.jsx
@@ -0,0 +1,23 @@
+// This is my component
+
+import React from "react";
+import useCounter from "./useCounter";
+import './ItemCount.css';
+
+const ItemCount = () => {
+
+  const { counter, increment, decrement, reset } = useCounter(1);
+
+  return (
+    <div className="counter">
+      <h2 className="counter_text">{counter}</h2>
+      <div className="counter_buttons">
+        <button className="decrement" onClick={decrement}>-</button>
+        <button onClick={reset}>reset</button>
+        <button className="increment" onClick={increment}>+</button>
+      </div>
+    </div>
+  );
+}
+
+export default ItemCount;
