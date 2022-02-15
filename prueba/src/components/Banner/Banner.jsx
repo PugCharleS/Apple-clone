1b2a2ad6ecc7457a3fd569db78259ce579417242
Carlos Solis <cmst010@gmail.com>
2022-02-15 14:05:08 -0700

    v1.6 Project Ended

--- prueba/src/components/Banner/Banner.jsx
+++ prueba/src/components/Banner/Banner.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import './Banner.css'
 
 export const Banner = ({ title, subtitle, img }) => {
@@ -6,12 +7,25 @@ export const Banner = ({ title, subtitle, img }) => {
       <div className="banner">
         <h1>{title}</h1>
         <h2>{subtitle}</h2>
+        <Link to='/category/iPhone' className="banner-link">Ver más &gt;</Link>
         <img src={img} alt={title} />
       </div>
     </>
   );
 }
 
+export const MacBanner = ({ title, subtitle, img }) => {
+  return (
+    <>
+      <div className="banner">
+        <h1>{title}</h1>
+        <h2>{subtitle}</h2>
+        <Link to='/category/Mac' className="banner-link">Ver más &gt;</Link>
+        <img src={img} alt={title} />
+      </div>
+    </>
+  );
+}
 
 export const NewBanner = ({ title, subtitle, model, img }) => {
   return (
@@ -20,6 +34,7 @@ export const NewBanner = ({ title, subtitle, model, img }) => {
       <h1>{title}</h1>
       <p className='model'>{model}</p>
       <h2 className='subtitle'>{subtitle}</h2>
+      <Link to='/category/Watch' className="banner-link">Ver más &gt;</Link>
       <img src={img} alt={title} />
     </div>
   );
@@ -33,6 +48,7 @@ export const BuyBanner = ({ title, subtitle, price, img, phrase }) => {
         <h1>{title}</h1>
         <h2>{subtitle}</h2>
         <p className='phrase'>{phrase}</p>
+        <Link to='/category/Mac' className="banner-link">Ver más &gt;</Link>
         <img src={img} alt={title} />
         <p className='price'>{price}</p>
       </div>
--- prueba/src/components/Home/Home.jsx
+++ prueba/src/components/Home/Home.jsx
@@ -1,11 +1,11 @@
-import { Banner, NewBanner } from '../Banner/Banner';
+import { Banner, NewBanner, MacBanner } from '../Banner/Banner';
 
 export const Home = () => {
   return (
     <>
       <Banner title="iPhone 13 Pro" subtitle="Muy muy Pro." img="https://www.prensalibre.com/wp-content/uploads/2021/09/iphone-13-1.jpg?quality=52"/>
       <NewBanner title="Apple Watch" subtitle="El fututo es a toda pantalla." model="SERIES 7" img="https://playjuego.com/wp-content/uploads/Apple-Watch-7-fecha-de-lanzamiento-precio-caracteristicas-y-fugas.png"/>
-      <Banner title="MacBook Pro" subtitle="Todo el poder para los Pro." img="https://www.apple.com/v/macbook-pro-14-and-16/b/images/meta/macbook-pro-14-and-16_overview__fz0lron5xyuu_og.png"/>
+      <MacBanner title="MacBook Pro" subtitle="Todo el poder para los Pro." img="https://www.apple.com/v/macbook-pro-14-and-16/b/images/meta/macbook-pro-14-and-16_overview__fz0lron5xyuu_og.png"/>
     </>
   )
 }
